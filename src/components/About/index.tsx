ffca6dc78ba012c96d87f0f1e663daffefc717e7
MantasValentin <mantasvalentin@protonmail.com>
2023-08-07 21:57:33 +0300

    update about me

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -11,18 +11,17 @@ const About = () => {
             Full-stack developer located in Lithuania
           </h3> */}
           <p className="text-lg max-sm:text-base">
-            As a web developer with a strong focus on TypeScript, I possess
-            extensive knowledge and proficiency in front-end and back-end
-            technologies. I have expertise in frameworks like React, Next, SCSS,
-            TailwindCSS, and Node, allowing me to design responsive websites
-            with clean, engaging interfaces through optimized, reusable code.
-            Additionally, I am skilled in creating and utilizing REST APIs and
-            working with databases such as MySQL and MongoDB. With experience in
-            Redis for data caching and Docker for streamlined deployment, I
-            ensure efficient system performance. As a quick learner and team
-            player, I thrive in collaborative environments and value receiving
-            feedback to continuously improve and adapt to any project's
-            requirements.
+            As a web developer focusing on TypeScript, I possess proficiency in
+            front-end and back-end technologies. I have experience in frameworks
+            like React, Next, SCSS, TailwindCSS, and Node, allowing me to design
+            responsive websites with clean, engaging interfaces through
+            optimized, reusable code. Additionally, I am capable of creating and
+            utilizing REST APIs and working with databases such as MySQL and
+            MongoDB. I ensure efficient system performance with experience in
+            Redis for data caching and Docker for streamlined deployment. As a
+            quick learner and team player, I thrive in collaborative
+            environments and value receiving feedback to improve and adapt to
+            any project's requirements continuously.
           </p>
         </div>
       </div>
