c32824b195f60fe799812b909828c126c0716630
MantasValentin <mantasvalentin@protonmail.com>
2023-08-14 18:18:19 +0300

    image optimization

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,25 +1,25 @@
-import css from "./tech/css.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
+import css from "./tech/css.webp";
+import git from "./tech/git.webp";
+import html from "./tech/html.webp";
+import javascript from "./tech/javascript.webp";
+import mongodb from "./tech/mongodb.webp";
+import nodejs from "./tech/nodejs.webp";
+import reactjs from "./tech/reactjs.webp";
+import tailwind from "./tech/tailwind.webp";
+import typescript from "./tech/typescript.webp";
 import threejs from "./tech/threejs.svg";
-import nextjs from "./tech/nextjs.png";
-import scss from "./tech/scss.png";
-import mysql from "./tech/mysql.png";
-import github from "./github.png";
-import docker from "./tech/docker.png";
-import graphql from "./tech/graphql.png";
-import postgresql from "./tech/postgresql.png";
-import redis from "./tech/redis.png";
+import nextjs from "./tech/nextjs.webp";
+import scss from "./tech/scss.webp";
+import mysql from "./tech/mysql.webp";
+import github from "./github.webp";
+import docker from "./tech/docker.webp";
+import graphql from "./tech/graphql.webp";
+import postgresql from "./tech/postgresql.webp";
+import redis from "./tech/redis.webp";
 
-import wondro from "./frontpage/wondro.png";
-import unregulated from "./frontpage/unregulated.png";
-import minesweeoer from "./frontpage/minesweeper.png";
+import wondro from "./frontpage/wondro.webp";
+import unregulated from "./frontpage/unregulated.webp";
+import minesweeoer from "./frontpage/minesweeper.webp";
 
 export {
   github,
